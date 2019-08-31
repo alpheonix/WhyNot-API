9c1ed437e3275ef1ccf79d149439bfa8a1f0d100
alpheonix <arthurblanc98@gmail.com>
2019-08-31 15:50:51 +0200

    dont display banned

--- routes/users/users.js
+++ routes/users/users.js
@@ -68,6 +68,7 @@ router.get('/', verifyToken, async (req, res, next) => {
                 film: req.token.film,
                 activite: req.token.activite,
                 musique: req.token.musique,
+                banned:false,
                 viewers: {
                     $not: {
                         $elemMatch: {
